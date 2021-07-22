313452921657a22923cf2512093e79c592569db1
Anisha Talati <talatianisha@gmail.com>
2021-07-22 15:14:34 +0100

    added magnemite

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a-scene
+        vr-mode-ui="enabled: false"
+        embedded
+        arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;"
+      >
+        <a-entity
+          gltf-model="./assets/magnemite/scene.gltf"
+          rotation="0 180 0"
+          scale="0.15 0.15 0.15"
+          gps-entity-place="longitude: 53.452161; latitude: -2.264759;"
+          animation-mixer
+        />
+        <a-camera gps-camera rotation-reader></a-camera>
+      </a-scene>
     </div>
   );
 }
