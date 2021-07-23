a68d0d5b5249c64f691ff333e2bf8c4c71738ed3
Anisha Talati <talatianisha@gmail.com>
2021-07-23 10:38:41 +0100

    playing with different ar

--- src/App.js
+++ src/App.js
@@ -9,13 +9,12 @@ function App() {
         embedded
         arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;"
       >
-        <a-entity
-          gltf-model="./assets/magnemite/scene.gltf"
-          rotation="0 180 0"
-          scale="0.15 0.15 0.15"
-          gps-entity-place="longitude: -2.264797; latitude: 53.452136;"
-          animation-mixer
-        />
+        <a-text
+          value="This will always face the user."
+          look-at="[gps-camera]"
+          scale="75 75 75"
+          gps-entity-place="latitude: 53.452136>; longitude: -2.264797;"
+        ></a-text>
         <a-camera gps-camera rotation-reader></a-camera>
       </a-scene>
     </div>
