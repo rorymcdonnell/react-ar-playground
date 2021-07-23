86517ed267a6b565b60e992105bb3dabd7231cb1
rorymcdonnell <rorymcdonnell95@gmail.com>
2021-07-23 15:29:15 +0200

    installed aframe-react

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import useGeolocation from "react-hook-geolocation";
+import { Scene } from "aframe-react";
 
 const Camera = () => {
   const geolocation = useGeolocation();
@@ -9,23 +10,23 @@ const Camera = () => {
         <h3 className="latitude">LATITUDE: {geolocation.latitude}</h3>
         <h3 className="longitude">LONGITUDE: {geolocation.longitude}</h3>
       </div>
-      <a-scene
-        vr-mode-ui="enabled: false"
-        embedded
-        arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;"
-      >
-        <a-text
-          value="This will always face the user."
-          look-at="[gps-camera]"
-          scale="75 75 75"
-          // gps-entity-place="latitude: 53.452136>; longitude: -2.264797;"
-          gps-entity-place={`latitude: ${geolocation.latitude}; longitude: ${geolocation.longitude};`}
-        ></a-text>
-        {console.log(
-          `latitude: ${geolocation.latitude}; longitude: ${geolocation.longitude}`
-        )}
-        <a-camera gps-camera rotation-reader></a-camera>
-      </a-scene>
+      <Scene>
+        <a-scene
+          vr-mode-ui="enabled: false"
+          embedded
+          arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;"
+        >
+          <a-text
+            value="This will always face the user."
+            look-at="[gps-camera]"
+            scale="75 75 75"
+            // gps-entity-place="latitude: 53.452136>; longitude: -2.264797;"
+            gps-entity-place={`latitude: ${geolocation.latitude}; longitude: ${geolocation.longitude};`}
+          ></a-text>
+
+          <a-camera gps-camera rotation-reader></a-camera>
+        </a-scene>
+      </Scene>
     </div>
   );
 };
